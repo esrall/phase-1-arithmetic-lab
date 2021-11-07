cfe6ecbf6072cac3b6a8aafad1aeab5d6f78bebd
Martha Solomon <marthsolomon@gmail.com>
2021-11-07 11:00:32 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const multiply = 62; 
+const num1 = 2
+const num2 = 31
 
-// Write your code here
+num1 * num2;
+
+const random = 1
+Math.floor(Math.random());
+
+const mod = 4;
+const num3 = 64
+const num4 = 10
+
+num3 % num4;
+
+const max = 20
+Math.max(1, 4, 7, 15, 17, 20);
